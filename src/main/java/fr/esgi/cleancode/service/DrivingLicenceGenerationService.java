78c3ca8278c52b4adb8a9db5a3b04de169c4461b
Brandon-Blackstar <mor.bra34@gmail.com>
2022-11-30 12:16:04 +0100

    Green tests Social Security Number

--- src/main/java/fr/esgi/cleancode/service/DrivingLicenceGenerationService.java
+++ src/main/java/fr/esgi/cleancode/service/DrivingLicenceGenerationService.java
@@ -1,10 +1,27 @@
 package fr.esgi.cleancode.service;
 
 
+import fr.esgi.cleancode.exception.InvalidDriverSocialSecurityNumberException;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class DrivingLicenceGenerationService {
 
     public void validateDriverSocialSecurityNumber(String driverSocialSecurityNumber) {
-
+        if (driverSocialSecurityNumber == null) {
+            throw new InvalidDriverSocialSecurityNumberException("Invalid Social Security Number is null");
+        }
+        Pattern patternIsAllNumbers = Pattern.compile("[0-9]+");
+        Pattern patternIsOk = Pattern.compile("[0-9]{15}");
+        Matcher matcherIsAllNumbers = patternIsAllNumbers.matcher(driverSocialSecurityNumber);
+        Matcher matcherIsOk = patternIsOk.matcher(driverSocialSecurityNumber);
+        if (!matcherIsAllNumbers.matches()) {
+            throw new InvalidDriverSocialSecurityNumberException("Invalid Social Security Number contains letter");
+        }
+        if (!matcherIsOk.matches()) {
+            throw new InvalidDriverSocialSecurityNumberException("Invalid Social Security Number size");
+        }
     }
 
 }
